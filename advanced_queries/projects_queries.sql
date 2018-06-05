ebb96fae4087923433f7b040dd1310e5f8b1b6a7
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-06-04 20:34:53 -0400

    questions 1-4

--- advanced_queries/projects_queries.sql
+++ advanced_queries/projects_queries.sql
@@ -1,14 +1,40 @@
 -- Create a new database called projects_db and restore it from the projects.sql file via the command:
 --    psql projects_db -f projects.sql
 
-
--- Write queries to answer the following questions:
+-- projects_db - id, name
+-- projects_uses_tech - project_id, tech_id
+-- tech - id, name
 
 -- 1. What are all projects that use JavaScript?
+
+SELECT project.name
+FROM projects_db
+INNER JOIN projects_uses_tech
+ON projects.id = project_uses_tech.project_id
+
 -- 2. What are all technologies used by the Personal Website?
+
+SELECT tech.name
+FROM tech
+INNER JOIN projects_db
+ON tech.id = projects_db.id
+
+
 -- 3. Perform a left outer join from the tech table to the project_uses_tech table - which techs has
 -- no associated project?
+
+SELECT tech.name
+FROM tech
+LEFT OUTER JOIN project_uses_tech
+ON tech.id = projects_uses_tech.tech_id
+
 -- 4. Based on the previous query, get the count of the number of techs used by each project.
+
+SELECT COUNT (tech.name) as total
+FROM tech
+LEFT OUTER JOIN project_uses_tech
+ON tech.id = projects_uses_tech.tech_id
+
 -- 5. Perform a left outer join from the project table to the project_users_tech table - which
 -- projects has no associated tech?
 -- 6. Based on the previous query, get the count of the number of projects that use each tech.
